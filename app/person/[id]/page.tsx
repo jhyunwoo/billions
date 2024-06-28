e207b317f61ba9ee96f2602b2f162f341f9d9e4f
jhyunwoo <jhyunwoo0228@gmail.com>
2024-06-29 01:16:07 +0900

    fix undefined data access error

--- app/person/[id]/page.tsx
+++ app/person/[id]/page.tsx
@@ -42,27 +42,27 @@ export default async function PersonDetailPage({ params }: { params: { id: strin
             className={'rounded-lg mb-4'}
           />
         ) : (
-          <div className={'w-full aspect-1 bg-slate-300 max-w-lg pb-4'}>
-            <div>No Image</div>
+          <div
+            className={
+              'w-full aspect-1 bg-slate-300 max-w-lg mb-4 flex items-center justify-center'
+            }
+          >
+            <div className={'text-4xl font-bold'}>No Image</div>
           </div>
         )}
         <div className={'text-2xl font-bold'}>{personData.name}</div>
         <div>
-          <div>Net Worth: {Math.round(Number(personData.netWorth) / 1000)} Billion</div>
-          <div>Country: {personData.country}</div>
-          <div>State: {personData.state}</div>
-          <div>City: {personData.city}</div>
+          <div>Net Worth: {Math.round(Number(personData?.netWorth) / 1000)} Billion</div>
+          <div>Country: {personData?.country}</div>
+          <div>State: {personData?.state}</div>
+          <div>City: {personData?.city}</div>
           <div className={'flex space-x-1'}>
             Industries:
-            {personData.industries.map((data, index) => (
-              <div key={index}> {data}</div>
-            ))}
+            {personData?.industries?.map((data, index) => <div key={index}> {data}</div>)}
           </div>
           <div className={'mt-4'}>
             <div className={'text-lg font-semibold'}>Bio</div>
-            {personData.bio.map((data, index) => (
-              <div key={index}>{data}</div>
-            ))}
+            {personData?.bio?.map((data, index) => <div key={index}>{data}</div>)}
           </div>
           <div className={'mt-4'}>
             <div className={'text-lg font-semibold'}>Financial Assets</div>
@@ -71,7 +71,7 @@ export default async function PersonDetailPage({ params }: { params: { id: strin
                 'w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-1'
               }
             >
-              {personData.financialAssets.map((data, index) => (
+              {personData?.financialAssets?.map((data, index) => (
                 <div key={index} className={'p-2 rounded-lg bg-slate-100'}>
                   <div className={'text-lg font-semibold'}>{data.companyName}</div>
                   <div>Exchange: {data.exchange}</div>
